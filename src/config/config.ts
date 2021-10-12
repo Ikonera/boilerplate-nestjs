26cefc72a70a63bc19fb612f10c362e1e258eae9
Gabriel <gabrielmlt@protonmail.ch>
2021-10-12 16:08:51 +0200

    Add .env flow

---
+++ src/config/config.ts
@@ -0,0 +1,5 @@
+export default () => ({
+	port: parseInt(process.env.APP_PORT, 10) || 4600,
+	// Add some stuff here...
+	
+})
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const appConfig: ConfigService = app.get(ConfigService)
+  const appPort: number = appConfig.get<number>('port')
+  await app.listen(appPort);
 }
 bootstrap();
